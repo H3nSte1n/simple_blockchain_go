3d54009277e390d10fa409112b6f65fb005bd31b
Henry Steinhauer <henry.steinhauer@i22.de>
2022-01-30 10:50:37 +0100

    feat: Improve naming and methods

--- data/block.go
+++ data/block.go
@@ -3,22 +3,37 @@ package data
 import (
 	"crypto/sha256"
 	"encoding/json"
-	"fmt"
 	"strconv"
-	"time"
+	"math/big"
+	"fmt"
 )
 
 type Block struct {
+	Nr 				int
 	Hash			string
 	PrevHash  string
-	Timestamp time.Time
+	Timestamp int64
 	Data      map[string]interface{}
-	PoW     int
+	Nonce     int
 }
 
-func (b Block) calculateHash() string {
+func (b Block) calculateHash() big.Int {
+	var bigInt big.Int
+	var blockHash [32]byte
+
 	data, _ := json.Marshal(b.Data)
-	blockData := b.PrevHash + string(data) + b.Timestamp.String() + strconv.Itoa(b.PoW) // b.Timestamp.String() == nounce
-	blockHash := sha256.Sum256([]byte(blockData))
-	return fmt.Sprintf("%x", blockHash)
+	blockData := b.PrevHash +
+		string(data) +
+		fmt.Sprint(b.Timestamp) +
+		strconv.Itoa(b.Nonce) +
+		strconv.Itoa(b.Nr)
+
+	blockHash = sha256.Sum256([]byte(blockData))
+
+	bigInt.SetBytes(blockHash[:])
+	return bigInt
+}
+
+func (b Block) validate(pow PoW) bool {
+	return pow.proof(b.calculateHash())
 }
--- data/blockchain.go
+++ data/blockchain.go
@@ -2,35 +2,58 @@ package data
 
 import (
 	"time"
-	"strings"
+	"math/big"
+	"fmt"
 )
 
 type Blockchain struct {
 	Blocks []Block
 	Genesis Block
-	Difficulty int
-}
-
-func (chain *Blockchain) SetDifficutly(difficulty int) {
-	chain.Difficulty = difficulty
-}
-
-func (chain Blockchain) mineBlock(b *Block) {
-	for !strings.HasPrefix(b.Hash, strings.Repeat("0", chain.Difficulty)) {
-		b.PoW++
-		b.Hash = b.calculateHash()
-	}
+	PoWs 		[]PoW
 }
 
 func (chain *Blockchain) AddBlock(data map[string]interface{}) {
+	currentPoW := chain.PoWs[len(chain.PoWs)-1]
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	newBlock := Block{
+		Nr: prevBlock.Nr + 1,
 		PrevHash: prevBlock.Hash,
-		Timestamp: time.Now(),
+		Timestamp: time.Now().Unix(),
 		Data: data,
-		PoW: 0,
+		Nonce: 0,
 	}
 
 	chain.mineBlock(&newBlock)
+	if !newBlock.validate(currentPoW) {
+		return
+	}
 	chain.Blocks = append(chain.Blocks, newBlock)
+
+	if len(chain.Blocks) % 2016 == 0 {
+		difficulty := *currentPoW.Difficulty
+		chain.AddPoW(difficulty)
+	}
+}
+
+func (chain Blockchain) mineBlock(b *Block) {
+	hash := b.calculateHash()
+	currentPoW := chain.PoWs[len(chain.PoWs)-1]
+
+	for !currentPoW.proof(hash) {
+		fmt.Println("mining")
+		b.Nonce++
+		b.Timestamp = time.Now().Unix()
+		hash = b.calculateHash()
+	}
+}
+
+func (chain *Blockchain) AddPoW(difficulty int) {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-difficulty))
+	newPoW := PoW{
+		Target: target,
+		Difficulty: &difficulty,
+	}
+
+	chain.PoWs = append(chain.PoWs, newPoW)
 }
