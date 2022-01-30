80fd18cc3ae95e042bffed07e18c8210262b8edf
Henry Steinhauer <henry.steinhauer@i22.de>
2022-01-30 10:54:44 +0100

    feat: Create entry point

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"blockchain/initialisation"
+)
 
+func main() {
+	fmt.Println("Init Blockchain")
+	chain := initialisation.CreateBlockchain()
+
+	chain.AddBlock(map[string]interface{}{"from": "82173817238781273817283", "to": "126376712637126312763", "amount": 4})
+	fmt.Println(chain)
 }
